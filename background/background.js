62790c1f69627572c176fdf7b74eb865eb077812
HellwalkerAlex <2022401@crieghtonprep.org>
2018-08-22 08:36:10 -0500

    Grass Commit
    Just adding grass.

---
+++ background/background.js
@@ -0,0 +1,17 @@
+function setup() {
+  
+  createCanvas(1280,1024);
+  background(255,255,255);
+  
+  //Framerate, for when I need that extra kick.
+  frameRate(75)
+  
+}
+
+function draw() {
+  
+  //Shapes
+  fill(19,133,16);
+  rect(0,718,1280,206);
+  
+}
